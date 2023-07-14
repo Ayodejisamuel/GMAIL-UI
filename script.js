76149d267afe5167225767c8539df822a7869a9c
Faleye Ayodeji <ayodejisamuel80@gmail.com>
2023-07-14 23:37:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,84 @@
- // 
+ document.addEventListener("DOMContentLoaded", function() {
+  let moreIcon = document.getElementById("moreIcon");
+  let toggleDiv = document.getElementById("toggleDiv");
+  let arrowDownIcon = document.getElementById("arrowdown");
+  let arrowDownDiv = document.getElementById("arrowdowndiv");
+  let helpIcon = document.getElementById("helpicon");
+  let helpDiv = document.getElementById("helpdiv");
+  let accountImg = document.getElementById("accountimg");
+  let accountDiv = document.getElementById("accountdiv");
 
-document.addEventListener("DOMContentLoaded", function() {
-    let moreIcon = document.getElementById("moreIcon");
-    let toggleDiv = document.getElementById("toggleDiv");
-    let arrowDownIcon = document.getElementById("arrowdown")
-    let arrowDownDiv = document.getElementById("arrowdowndiv")
-    let helpIcon = document.getElementById("helpicon")
-    let helpDiv = document.getElementById("helpdiv")
-    let accountImg = document.getElementById("accountimg")
-    let accountDiv = document.getElementById("accountdiv")
+  moreIcon.addEventListener("click", () => {
 
-
-  
-moreIcon.addEventListener("click", () => {
-  
-      toggleDiv.classList.toggle("hidden");
-    }); 
-
-
-  arrowDownIcon.addEventListener('click', () => {
-
-    arrowDownDiv.classList.toggle('hidden')
-
-  })
-
-  helpIcon.addEventListener('click', () => {
-    helpDiv.classList.toggle("hidden")
+    toggleDiv.classList.toggle("hidden");
+    
   });
 
+  document.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    if (
+      clickedElement !== toggleDiv &&
+      !toggleDiv.contains(clickedElement) &&
+      clickedElement !== moreIcon
+    ) {
+    toggleDiv.classList.add("hidden");
 
-  accountImg.addEventListener('click', () => {
-    accountDiv.classList.toggle('hidden')
-  })
-
+    } 
 
   });
+
+  arrowDownIcon.addEventListener("click", () => {
+    arrowDownDiv.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    if (
+      clickedElement !== arrowDownDiv &&
+      !arrowDownDiv.contains(clickedElement) &&
+      clickedElement !== arrowDownIcon
+    ) {
+    arrowDownDiv.classList.add("hidden");
+
+    } 
+
+  });
+
+  helpIcon.addEventListener("click", () => {
+    helpDiv.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    if (
+      clickedElement !== helpDiv &&
+      !helpDiv.contains(clickedElement) &&
+      clickedElement !== helpIcon
+    ) {
+    helpDiv.classList.add("hidden");
+
+    } 
+
+  });
+
+  accountImg.addEventListener("click", () => {
+    accountDiv.classList.toggle("hidden");
+  });
+
+  document.addEventListener("click", (event) => {
+    const clickedElement = event.target;
+    if (
+      clickedElement !== accountDiv &&
+      !accountDiv.contains(clickedElement) &&
+      clickedElement !== accountImg
+    ) {
+    accountDiv.classList.add("hidden");
+
+    } 
+
+  });
+
+  
+
+  
+});
